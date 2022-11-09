cfcb13fec8cf3538282f72e580b5956dd77f576b
ManvelManucharyan <112545060+ManvelManucharyan@users.noreply.github.com>
2022-11-09 14:29:17 +0400

    first

---
+++ index.js
@@ -0,0 +1,8 @@
+require("dotenv").config()
+const TelegramApi = require("node-telegram-bot-api");
+
+const bot = new TelegramApi(process.env.TOKEN, { polling: true });
+
+bot.on('message', msg=> {
+    console.log(msg);
+})
