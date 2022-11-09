75f4201acfaf757ed25021bec1b17464f90af866
Manvel Manucharyan <manvelmanucharyan98@gmail.com>
2022-11-10 00:28:34 +0400

    routes

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
-require("dotenv").config()
+require("dotenv").config();
 const TelegramApi = require("node-telegram-bot-api");
+const Text = require("./routes/text");
 
 const bot = new TelegramApi(process.env.TOKEN, { polling: true });
 
-function start () {
-    bot.setMyCommands([
-        {command: "/start", description: "Start"},
-        {command: "/info", description: "Your name and surname"}
-    ])
-    
-    bot.on('message', async msg=> {
-        const text = msg.text
-        const chatId = msg.chat.id
-        if (text === "/start") {
-            await bot.sendSticker(chatId, "https://tlgrm.eu/_/stickers/4dd/300/4dd300fd-0a89-3f3d-ac53-8ec93976495e/1.webp")
-            return await bot.sendMessage(chatId, `Welcome to ankap bot`);
-        }
-        if (text === "/info") {
-            return await bot.sendMessage(chatId, `Your name is ${msg.from.first_name} ${msg.from.last_name}`)
-        }
-        return await bot.sendMessage(chatId, `I dont understand`)
+function run() {
+  bot.setMyCommands([
+    { command: "/start", description: "Start" },
+    { command: "/info", description: "Your name and surname" },
+    { command: "/film", description: "Your faivorite film" },
+  ]);
 
-    })
+  bot.on("message", async (msg) => {
+    const text = msg.text;
+    if (text === "/start") {
+      Text.start(msg, bot);
+    } else if (text === "/info") {
+        Text.info(msg, bot)
+    } else if (text === "/film") {
+        Text.film(msg, bot)
+    } else {
+      return await bot.sendMessage(chatId, `I dont understand`);
+    }
+  });
 }
 
-start()
+run();
